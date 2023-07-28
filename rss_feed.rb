57e0de64e2898104b06751b19dae0ab823794956
Copy-Zero <copy.zero.division@gmail.com>
2023-07-28 12:15:14 -0700

    Added Search function and user menu

--- rss_feed.rb
+++ rss_feed.rb
@@ -1,27 +1,45 @@
-require "rss"
-require "open-uri"
+require 'rss'
+require 'open-uri'
 
 def get_main_episodes(episodes)
   main_episodes = []
   episodes.each do |episode|
-    if episode.title.include? "Episode"
-      main_episodes << [episode.title, episode.pubDate]
-    end
+    main_episodes << [episode.title, episode.pubDate] if episode.title.include? 'Episode'
   end
   main_episodes
 end
 
-lpotl_url = "http://feeds.feedburner.com/TheLastPodcastOnTheLeft"
+def recent_five(episodes)
+  puts '=================== Top Five ===================='
+  5.times do |index|
+    puts episodes[index][0]
+    puts episodes[index][1]
+  end
+end
+
+def search_episodes(episodes)
+  puts '===================== Search ====================='
+  puts 'Enter Search string: '
+  search = gets.chomp
+
+  puts(episodes.select { |episode| episode[0].include? search })
+end
+
+lpotl_url = 'http://feeds.feedburner.com/TheLastPodcastOnTheLeft'
 URI.open(lpotl_url) do |rss|
   feed = RSS::Parser.parse(rss)
   puts "Title: #{feed.channel.title}"
-  main_epsisodes = get_main_episodes(feed.items)
-  5.times do |index|
-    puts main_epsisodes[index][0]
-    puts main_epsisodes[index][1]
-    
-  end
+  main_episodes = get_main_episodes(feed.items)
 
+  # menu
+  puts 'LPOTL RSS Feed  ------'
+  puts '  Option 1: 5 Most Recent Episodes'
+  puts '  Option 2: Search in episode titles'
+
+  user_choice = gets.chomp
+
+  recent_five(main_episodes) if user_choice == '1'
+  search_episodes(main_episodes) if user_choice == '2'
   # feed.items.each do |item|
   # puts "Item: #{item.title}"
   # end
