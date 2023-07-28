cffd7785742c97652a1a63cb8ef7223361604d45
Copy-Zero <copy.zero.division@gmail.com>
2023-07-28 09:55:32 -0700

    Added Get Main Eps Function

--- rss_feed.rb
+++ rss_feed.rb
@@ -1,11 +1,28 @@
 require "rss"
 require "open-uri"
 
+def get_main_episodes(episodes)
+  main_episodes = []
+  episodes.each do |episode|
+    if episode.title.include? "Episode"
+      main_episodes << [episode.title, episode.pubDate]
+    end
+  end
+  main_episodes
+end
+
 lpotl_url = "http://feeds.feedburner.com/TheLastPodcastOnTheLeft"
 URI.open(lpotl_url) do |rss|
   feed = RSS::Parser.parse(rss)
   puts "Title: #{feed.channel.title}"
-  feed.items.each do |item|
-    puts "Item: #{item.title}"
+  main_epsisodes = get_main_episodes(feed.items)
+  5.times do |index|
+    puts main_epsisodes[index][0]
+    puts main_epsisodes[index][1]
+    
   end
+
+  # feed.items.each do |item|
+  # puts "Item: #{item.title}"
+  # end
 end
